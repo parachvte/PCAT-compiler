62ec806ca6326a5b82833ba1aae923cc574b5ac9
Ryannnnnnn <ryannx6@gmail.com>
2014-06-14 22:09:58 +0800

    delete global

--- src/code_gen.c
+++ src/code_gen.c
@@ -4,7 +4,6 @@
 #include <string.h>
 
 #include "routine.h"
-#include "global.h"
 #include "table.h"
 #include "ast.h"
 //#include "type_check.h"
--- src/global.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GLOBAL_H_
-#define _GLOBAL_H_
-
-#define DEBUG
-
-void debug_print(int dbg_lvl, char *fmt, ...);
-
-#endif
